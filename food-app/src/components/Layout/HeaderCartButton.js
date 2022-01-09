4d949e8b39c0d0e991c7e3d041c515c6b07684a9
Iolanta26 <iolanta.u@gmail.com>
2022-01-09 20:06:56 +0200

    food app half done

---
+++ food-app/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.length;
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
